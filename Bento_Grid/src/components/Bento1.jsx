2daaa318576a419f8603db8b22bbe53156fd9772
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-07 20:16:35 +0530

    Update Bento1.jsx

--- Bento_Grid/src/components/Bento1.jsx
+++ Bento_Grid/src/components/Bento1.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Bento1 = () => {
   return (
     <>
-    <div className='h-screen w-full flex justify-center items-center'>hi</div>
+    <div className='h-screen w-full flex justify-center items-center'>
+        <div className='grid grid-cols-10 grid-rows-4 gap-10 h-full w-full'>
+            
+        </div>
+    </div>
     </>
   )
 }
