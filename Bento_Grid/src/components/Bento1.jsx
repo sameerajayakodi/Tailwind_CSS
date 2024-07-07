c8210e92f00db5cbc51bc44ef733f4cd3a314b25
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-07 21:07:29 +0530

    Update Bento1.jsx

--- Bento_Grid/src/components/Bento1.jsx
+++ Bento_Grid/src/components/Bento1.jsx
@@ -4,14 +4,14 @@ const Bento1 = () => {
   return (
     <>
     <div className='p-6 h-screen w-full flex justify-center items-center'>
-        <div className='grid grid-cols-10 grid-rows-4 gap-10 h-full w-full'>
-            <div className='col-span-4 row-span-3 bg-indigo-200 flex rounded-3xl justify-center items-center'></div>
-            <div className='col-span-4 row-span-1 bg-indigo-200 flex rounded-3xl justify-center items-center'></div>
-            <div className='col-span-2 row-span-1 bg-indigo-200 flex rounded-3xl justify-center items-center'></div>
-            <div className='col-span-3 row-span-2 bg-indigo-200 flex rounded-3xl justify-center items-center'></div>
-            <div className='col-span-3 row-span-2 bg-indigo-200 flex rounded-3xl justify-center items-center'></div>
-            <div className='col-span row-span bg-indigo-200 flex rounded-3xl justify-center items-center'></div>
-            <div className='col-span row-span bg-indigo-200 flex rounded-3xl justify-center items-center'></div>
+        <div className='grid grid-cols-10 grid-rows-4 gap-6 h-full w-full'>
+            <div className='col-span-4 row-span-3 bg-indigo-200 flex rounded-2xl justify-center items-center'></div>
+            <div className='col-span-4 row-span-1 bg-indigo-200 flex rounded-2xl justify-center items-center'></div>
+            <div className='col-span-2 row-span-1 bg-indigo-200 flex rounded-2xl justify-center items-center'></div>
+            <div className='col-span-3 row-span-2 bg-indigo-200 flex rounded-2xl justify-center items-center'></div>
+            <div className='col-span-3 row-span-2 bg-indigo-200 flex rounded-2xl justify-center items-center'></div>
+            <div className='col-span-4 row-span bg-indigo-200 flex rounded-2xl justify-center items-center'></div>
+            <div className='col-span-6 row-span bg-indigo-200 flex rounded-2xl justify-center items-center'></div>
             
         </div>
     </div>
