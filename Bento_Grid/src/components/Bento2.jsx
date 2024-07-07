949e22d8c42885eee07940295af441dca20c9ced
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-07 22:16:09 +0530

    Update Bento2.jsx

--- Bento_Grid/src/components/Bento2.jsx
+++ Bento_Grid/src/components/Bento2.jsx
@@ -8,9 +8,9 @@ const Bento2 = () => {
                 <div className='bg-indigo-300 col-span-1 row-span-1 justify-center items-center flex '>0</div>
                 <div className='bg-indigo-300 col-span-6 row-span-1 justify-center items-center flex rounded-3xl'>1</div>
                 <div className='bg-indigo-300 col-span-1 row-span-1 justify-center items-center flex rounded-full border'>2</div>
-                <div className='bg-indigo-300 col-span-6 row-span-6 justify-center items-center flex rounded-2xl'>3</div>
+                <div className='bg-indigo-300 col-span-6 row-span-7 justify-center items-center flex rounded-2xl'>3</div>
                 <div className='bg-indigo-300 col-span-2 row-span-7 justify-center items-center flex rounded-lg'>4</div>
-                <div className='bg-indigo-300 col-span-6 row-span-1 justify-center items-center flex rounded-2xl'>5</div>
+               
                 <div className='bg-indigo-300 col-span-4 row-span-2 justify-center items-center flex rounded-2xl'>6</div>
                 <div className='bg-indigo-300 col-span-2 row-span-2 justify-center items-center flex rounded-2xl'>7</div>
                 <div className='bg-indigo-300 col-span-2 row-span-2 justify-center items-center flex rounded-2xl'>8</div>
