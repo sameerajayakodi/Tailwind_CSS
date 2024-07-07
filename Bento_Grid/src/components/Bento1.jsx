2b867147a66d775a2597ec567cb2bc9f14008e7f
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-07 21:03:34 +0530

    Update Bento1.jsx

--- Bento_Grid/src/components/Bento1.jsx
+++ Bento_Grid/src/components/Bento1.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Bento1 = () => {
   return (
     <>
-    <div className='h-screen w-full flex justify-center items-center'>
+    <div className='p-6 h-screen w-full flex justify-center items-center'>
         <div className='grid grid-cols-10 grid-rows-4 gap-10 h-full w-full'>
             <div className='col-span-4 row-span-3 bg-indigo-200 flex rounded-3xl justify-center items-center'></div>
-            <div className='col-span row-span bg-indigo-200 flex rounded-3xl justify-center items-center'></div>
-            <div className='col-span row-span bg-indigo-200 flex rounded-3xl justify-center items-center'></div>
-            <div className='col-span row-span bg-indigo-200 flex rounded-3xl justify-center items-center'></div>
+            <div className='col-span-4 row-span-1 bg-indigo-200 flex rounded-3xl justify-center items-center'></div>
+            <div className='col-span-1 row-span-1 bg-indigo-200 flex rounded-3xl justify-center items-center'></div>
+            <div className='col-span-2 row-span-2 bg-indigo-200 flex rounded-3xl justify-center items-center'></div>
             <div className='col-span row-span bg-indigo-200 flex rounded-3xl justify-center items-center'></div>
             <div className='col-span row-span bg-indigo-200 flex rounded-3xl justify-center items-center'></div>
             <div className='col-span row-span bg-indigo-200 flex rounded-3xl justify-center items-center'></div>
