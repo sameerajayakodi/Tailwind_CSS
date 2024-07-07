2f47c4020645ba2ee03aaa767d07a7e19dafcb5f
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-07 21:25:57 +0530

    Update Bento2.jsx

--- Bento_Grid/src/components/Bento2.jsx
+++ Bento_Grid/src/components/Bento2.jsx
@@ -4,7 +4,19 @@ const Bento2 = () => {
   return (
     <>
       <div className='w-full h-screen flex justify-center items-center p-6 bg-slate-200 '>
-            <div className='grid grid-cols-8 grid-rows-12 gap-6 h-full w-full'></div>
+            <div className='grid grid-cols-8 grid-rows-12 gap-6 h-full w-full'>
+                <div className='bg-slate-700 col-span-1 row-span-1'></div>
+                <div className='bg-slate-700 col-span-1 row-span-1'></div>
+                <div className='bg-slate-700 col-span-1 row-span-1'></div>
+                <div className='bg-slate-700 col-span-1 row-span-1'></div>
+                <div className='bg-slate-700 col-span-1 row-span-1'></div>
+                <div className='bg-slate-700 col-span-1 row-span-1'></div>
+                <div className='bg-slate-700 col-span-1 row-span-1'></div>
+                <div className='bg-slate-700 col-span-1 row-span-1'></div>
+                <div className='bg-slate-700 col-span-1 row-span-1'></div>
+                <div className='bg-slate-700 col-span-1 row-span-1'></div>
+                <div className='bg-slate-700 col-span-1 row-span-1'></div>
+            </div>
       </div>
     </>
   )
